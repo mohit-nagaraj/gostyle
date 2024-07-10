64c7f97c4449df1f7cd2263162ca0b04c7c36c14
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-07-10 18:00:00 +0530

    Just in time print
    It reads one rune at a time from os.Stdin and adds it to the output slice of runes. The output rendering has been extracted to print(), but we could also pipe 'just in time' each rune as it’s scanned

--- main.go
+++ main.go
@@ -15,17 +15,8 @@ func rgb(i int) (int, int, int) {
 		int(math.Sin(f*float64(i)+4*math.Pi/3)*127 + 128)
 }
 
-func print(output []rune) {
-	for j := 0; j < len(output); j++ {
-		r, g, b := rgb(j)
-		fmt.Printf("\033[38;2;%d;%d;%dm%c\033[0m", r, g, b, output[j])
-	}
-	fmt.Println()
-}
-
 func main() {
 	info, _ := os.Stdin.Stat()
-	var output []rune
 
 	if info.Mode()&os.ModeCharDevice != 0 {
 		fmt.Println("The command is intended to work with pipes.")
@@ -33,13 +24,14 @@ func main() {
 	}
 
 	reader := bufio.NewReader(os.Stdin)
+	j := 0
 	for {
 		input, _, err := reader.ReadRune()
 		if err != nil && err == io.EOF {
 			break
 		}
-		output = append(output, input)
+		r, g, b := rgb(j)
+		fmt.Printf("\033[38;2;%d;%d;%dm%c\033[0m", r, g, b, input)
+		j++
 	}
-
-	print(output)
 }
