ef9670d2d8c69f3897e413b15cbf3ff8da8c95a1
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-07-12 04:54:00 +0530

    read fortunes
    simple program that display a pseudorandom message from a database of quotations

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/mohit-nagaraj/gowow
 
 go 1.22.3
 
-require syreclabs.com/go/faker v1.2.3 // indirect
+require github.com/alecthomas/kingpin/v2 v2.4.0
+
+require (
+	github.com/alecthomas/units v0.0.0-20211218093645-b94a6e3cc137 // indirect
+	github.com/xhit/go-str2duration/v2 v2.1.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,37 +1,84 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"io"
-	"math"
+	"log"
+	"math/rand"
 	"os"
+	"strings"
+	"time"
+
+	"github.com/alecthomas/kingpin/v2"
 )
 
-func rgb(i int) (int, int, int) {
-	var f = 0.1
-	return int(math.Sin(f*float64(i)+0)*127 + 128),
-		int(math.Sin(f*float64(i)+2*math.Pi/3)*127 + 128),
-		int(math.Sin(f*float64(i)+4*math.Pi/3)*127 + 128)
+var (
+	filename = kingpin.Arg("filename", "Fortune filename").Default(".").String()
+)
+
+// Read filename from command line, and check if this filename is file, after
+// that  just printFortune. If filenmae is directory, we get all filenames from
+// directory and check if this file is fortune we adding filename to slice.
+// After that we get random filename and then printForune with this filename.
+func main() {
+	kingpin.Version("0.0.1")
+	kingpin.Parse()
+
+	fi, err := os.Stat(*filename)
+	if err != nil {
+		log.Fatal(*filename + " is not exist...")
+	}
+	if fi.Mode().IsRegular() {
+		if isFortune(*filename) {
+			printFortune(*filename)
+		}
+	} else if fi.Mode().IsDir() {
+		files, err := os.ReadDir(*filename)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var forts []string
+		for _, file := range files {
+			if isFortune(*filename + "/" + file.Name()) {
+				forts = append(forts, *filename+"/"+file.Name())
+			}
+		}
+		s1 := rand.NewSource(time.Now().UnixNano())
+		r1 := rand.New(s1)
+		rnd := r1.Intn(len(forts))
+		printFortune(forts[rnd])
+	}
 }
 
-func main() {
-	info, _ := os.Stdin.Stat()
-
-	if info.Mode()&os.ModeCharDevice != 0 {
-		fmt.Println("The command is intended to work with pipes.")
-		fmt.Println("Usage: fortune | gorainbow")
+// First version printFortune.
+// Just read full file to memory, after that split to '/n%/n' and
+// printing random fortunes.
+//
+// TODO We need to change this to reading a part of file.
+func printFortune(filename string) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatal("Cannot open " + filename + " filename...")
 	}
 
-	reader := bufio.NewReader(os.Stdin)
-	j := 0
-	for {
-		input, _, err := reader.ReadRune()
-		if err != nil && err == io.EOF {
-			break
-		}
-		r, g, b := rgb(j)
-		fmt.Printf("\033[38;2;%d;%d;%dm%c\033[0m", r, g, b, input)
-		j++
+	fortunes := strings.Split(string(data), "\n%\n")
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+	rnd := r1.Intn(len(fortunes))
+
+	fmt.Println(fortunes[rnd])
+}
+
+// If we have filename and file with filename.dat, we then we think that this
+// fortune file.
+// We check that the filename exists, and filename.dat exist too
+func isFortune(filename string) bool {
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		return false
 	}
+
+	if _, err := os.Stat(filename + ".dat"); os.IsNotExist(err) {
+		return false
+	}
+	return true
 }
