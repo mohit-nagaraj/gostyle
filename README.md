99ec5d86677f2106225e5a412bec31090d076c24
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-07-17 04:54:00 +0530

    update main
    finish integration and added docs

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# gostyle
-stylish terminal applications for lolcat
+# Fortune Cow Rainbow
+
+Fortune Cow Rainbow is a fun project that generates a random fortune, formats it with a cow image, colors it with RGB values, and prints it to the console.
+
+### Project Structure
+* main.go: The main program logic to generate a random fortune.
+* fortunes/: A directory containing fortune files.
+* cowsay/cow.go: A package to create text balloons and display a cow image.
+* catlol/cat.go: A package to color the text with a rainbow effect.
+
+## Usage
+### Running the Project
+You can run the project with the following command:
+
+```
+go run main.go
+```
+<br>
+
+### Building and Installing
+To build and install the project, follow these steps:
+
+1. Clone the repository:
+
+    ```
+    git clone https://github.com/mohit-nagaraj/gowow.git
+    cd fortune-cow-rainbow
+    ```
+2. Build the project:
+
+    ```
+    go build -o gowow main.go
+    ```
+Run the built executable
+
+```main.go```
+
+The main.go file reads fortune files from the fortunes directory, selects a random fortune, and prints it. If the specified filename is a directory, it reads all files from the directory, checks if they are fortune files, and selects one randomly to print.
+
+```cowsay/cow.go```
+
+This file creates a text balloon with the fortune and appends a cow image below the text. It processes the input text, adds borders, and normalizes the string lengths to ensure proper alignment within the balloon.
+
+```catlol/cat.go```
+
+This file colors the input text using a rainbow effect. It calculates the RGB values for each character and prints the colored text to the console.
+
+## Contributing
+Feel free to fork the repository, create a branch, and submit a pull request with your improvements.
+
+## License
+This project is licensed under the MIT License - see the LICENSE file for details.
--- main.go
+++ main.go
@@ -9,71 +9,73 @@ import (
 	"time"
 
 	"github.com/alecthomas/kingpin/v2"
+	"github.com/mohit-nagaraj/gowow/catlol"
+	"github.com/mohit-nagaraj/gowow/cowsay"
 )
 
 var (
 	filename = kingpin.Arg("filename", "Fortune filename").Default(".").String()
 )
 
-// Read filename from command line, and check if this filename is file, after
-// that  just printFortune. If filenmae is directory, we get all filenames from
-// directory and check if this file is fortune we adding filename to slice.
-// After that we get random filename and then printForune with this filename.
+// main is the entry point of the application. It sets up the command line arguments,
+// verifies the existence of the fortunes directory, selects a random fortune file,
+// retrieves a random fortune from the file, and then formats and displays it using
+// a cow image with colored text.
 func main() {
 	kingpin.Version("0.0.1")
 	kingpin.Parse()
 	const fortunesDir = "fortunes"
 	fi, err := os.Stat(fortunesDir)
 	if err != nil {
-		log.Fatal(fortunesDir + " is not exist...")
+		log.Fatal(fortunesDir + " does not exist...")
 	}
-	// if fi.Mode().IsRegular() {
-	// 	if isFortune(*filename) {
-	// 		printFortune(*filename)
-	// 	}
-	// } else
+	// Check if the fortunesDir is a directory.
 	if fi.Mode().IsDir() {
-		files, err := os.ReadDir(fortunesDir)
+		files, err := os.ReadDir(fortunesDir) // Read the contents of the directory.
 		if err != nil {
 			log.Fatal(err)
 		}
 
 		var forts []string
 		for _, file := range files {
-			filePath := fortunesDir + "/" + file.Name()
+			filePath := fortunesDir + "/" + file.Name() // Check if the file is a fortune file.
 			if isFortune(filePath) {
 				forts = append(forts, filePath)
 			}
 		}
-		s1 := rand.NewSource(time.Now().UnixNano())
-		r1 := rand.New(s1)
+		s1 := rand.NewSource(time.Now().UnixNano()) // Select a random fortune file.
+		r1 := rand.New(s1)                          // Retrieve a random fortune from the file.
 		rnd := r1.Intn(len(forts))
-		printFortune(forts[rnd])
+		fortune := getFortune(forts[rnd])
+		balloon := cowsay.BuildBalloon(fortune)                 //display cow
+		rainbow := catlol.MakeRainbowText(balloon + cowsay.Cow) //color the text in rainbow colors.
+		fmt.Println(rainbow)
 	}
 }
 
-// First version printFortune.
-// Just read full file to memory, after that split to '/n%/n' and
-// printing random fortunes.
+// Reads the content of a fortune file, splits it into individual fortunes,
+// and returns a random fortune.
 //
-// TODO We need to change this to reading a part of file.
-func printFortune(filename string) {
+// filename: The path to the fortune file.
+func getFortune(filename string) string {
 	data, err := os.ReadFile(filename)
 	if err != nil {
 		log.Fatal("Cannot open " + filename + " filename...")
 	}
-
+	// Split the content by "\n%\n" to separate individual fortunes.
 	fortunes := strings.Split(string(data), "\n%\n")
+	// Select a random fortune.
 	s1 := rand.NewSource(time.Now().UnixNano())
 	r1 := rand.New(s1)
 	rnd := r1.Intn(len(fortunes))
 
-	fmt.Println(fortunes[rnd])
+	return fortunes[rnd]
 }
 
-// If we have filename and file with filename.dat, we then we think that this
-// fortune file.
-// We check that the filename exists, and filename.dat exist too
+// Checks if a given filename is a valid fortune file. A valid fortune file
+// has a corresponding ".dat" file.
+//
+// filename: The name of the file to check.
 func isFortune(filename string) bool {
 	if _, err := os.Stat(filename); os.IsNotExist(err) {
 		return false
